0643830664d14a50a7b918ae09ba1ddfc56535f3
Derek <55922076+SweepyHobo@users.noreply.github.com>
2023-07-05 12:22:53 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# SweepyHobo
 # Input:
 # p, principal amount
 # t, time period in years
